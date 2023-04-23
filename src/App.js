0e803feae36b43feaa55901a2d4f8c911b9945f7
HP <callmesammy@yahoo.com>
2023-04-23 11:27:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold t">
+    <h1 className="text-3xl font-bold text-sub">
       Hello world!
     </h1>
   )
