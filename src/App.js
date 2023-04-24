e3ed6453bf9f3af75dc8d8271319374e28385163
HP <callmesammy@yahoo.com>
2023-04-24 11:02:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom';
+import Homepage from './screens/Homepage';
+import Aboutus from './screens/Aboutus';
+import NotFound from './screens/NotFound';
 
 function App() {
   return (
-    <div className=" h-header bg-maint w-full"> 
-    <h1 className="text-3xl font-bold  text-sub">
-      Hello world!
-    </h1>
-    <p>
-      hi
-    </p>
-    </div>
-  )
+   <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/aboutus" element={<Aboutus />} />
+      <Route path="*" element={<NotFound />} />
+   </Routes>
+  );
 } 
 
 export default App
