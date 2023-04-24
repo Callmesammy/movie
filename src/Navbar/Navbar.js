66717b18a58146ab23ac91168cc972b8f3c788a3
HP <callmesammy@yahoo.com>
2023-04-24 11:22:07 +0100

    Update

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <div className="bg-maint shadow-md sticky top-0 z-0">
+        <div className="container bg-sub mx-auto lg:grid gap-10 grid-cols-7 justify-between items-center py-2 px-4">
+        <p>Navbar</p>
+        </div> 
+        </div>
+    </>
   )
 }
 
--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Layout from '../Layout/Layout'
+
 
 function Homepage() {
   return (
+    <Layout>
     <div>Homepage</div>
-  )
+    </Layout>
+  );
 }
 
 export default Homepage
