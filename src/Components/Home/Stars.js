ca0def613d60a830a33bb2093122616c9af9247a
HP <callmesammy@yahoo.com>
2023-04-27 04:32:51 +0100

    up

--- src/Components/Home/Stars.js
+++ src/Components/Home/Stars.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa'
+
 
 
 
@@ -6,17 +8,68 @@ import React from 'react'
 function Stars ({value}) {
   return (
     <>
-   
+  
     <span>
         {
-            value >= 5 ? (
+            value >= 1 ? (
                 <FaStar/>
-            ) : value >= 4.5 ? 
+            ) : value >= 1.5 ? 
             (<FaStarHalfAlt/>
             ) : (<FaRegStar/>)
           
             }  
     </span>
+    <span>
+        {
+            value >= 2 ? (
+                <FaStar/>
+            ) : value >= 2.5 ? 
+            (<FaStarHalfAlt/>
+            ) : (<FaRegStar/>)
+          
+            }  
+    </span>
+    <span>
+        {
+            value >= 2 ? (
+                <FaStar/>
+            ) : value >= 2.5 ? 
+            (<FaStarHalfAlt/>
+            ) : (<FaRegStar/>)
+          
+            }  
+    </span>
+    <span>
+        {
+            value >= 3 ? (
+                <FaStar/>
+            ) : value >= 3.5 ? 
+            (<FaStarHalfAlt/>
+            ) : (<FaRegStar/>)
+          
+            }  
+    </span>
+    <span>
+        {
+            value >= 4 ? (
+                <FaStar/>
+            ) : value >= 4.6 ? 
+            (<FaStarHalfAlt/>
+            ) : (<FaRegStar/>)
+          
+            }  
+    </span>
+    <span>
+        {
+            value >= 5 ? (
+                <FaStar/>
+            ) : value >= 5.5 ? 
+            (<FaStarHalfAlt/>
+            ) : (<FaRegStar/>)
+          
+            }  
+    </span>
+
     </>
     )
 }
--- src/Components/Home/TopRated.js
+++ src/Components/Home/TopRated.js
@@ -1,13 +1,17 @@
 
 import React, {useState} from 'react'
 import Title from '../Title'
-import { BsCollectionFill } from 'react-icons/bs'
+import { BsCaretLeftFill, BsCaretRightFill, BsCollectionFill } from 'react-icons/bs'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import { Movies } from './../../Data/MovieData';
 import { Autoplay } from 'swiper';
 import { FaHeart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Stars from './Stars';
+
+
+
 
 
 
@@ -16,6 +20,8 @@ function TopRated() {
 
   const [nextE1, setnextE1] = useState(null);
   const [prevE1, setprevE1] = useState(null);
+
+  const classNames= "hover:bg-dyr transitions rounded text-sm w-8 h-8 flex-colo bg-sub text-white";
 return (
 <div className="my-16">
       <Title title = "Top Rated " Icon = {BsCollectionFill}/>
@@ -30,11 +36,10 @@ return (
           {
             Movies.map((movie, index) => (
                 <SwiperSlide key={index}>
-                <div className=" p-4 h-rate border border-brd bg-dyr rounded-lg overflow-hidden">
+                <div className=" p-4 h-rate hovered border border-brd bg-dyr rounded-lg overflow-hidden">
                   <img src={movie.titleImage} alt={movie.titleImage} 
                   className="w-full h-full object-cover rounded-lg"/>
-                </div>
-                <div className="px-4 flex-colo text-center absolute bg-black bg-opacity-70 top-0 bottom-0 left-0 right-0">
+                <div className="px-4 hoveres text-center absolute bg-black bg-opacity-70 top-0 bottom-0 left-0 right-0">
                   <button className="  w-12 h-12 flex-colo transitions hover:bg-sub rounded-full bg-opacity-30 text-white">
                     <FaHeart/>                  </button>
 
@@ -42,12 +47,22 @@ return (
                       {movie.name}
                       </Link>
                   <div className="flex gap-2 text-str">
+                    <Stars value={movie.rate}/>
                   </div>
-
+                  </div>
                 </div>
                 </SwiperSlide>
+                
          )) }
         </Swiper>
+        <div className="w-full px-1 flex-rows gap-6 pt-12">
+          <button className={classNames} ref={(node) => setprevE1(node)}>
+            <BsCaretLeftFill/>
+            </button>
+          <button className={classNames} ref={(node) => setprevE1(node)}>
+            <BsCaretRightFill/>
+            </button>
+          </div>
       </div>
 </div>
     );
