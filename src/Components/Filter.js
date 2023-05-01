37ad974e05b79a19b22a77ea28deda54500571e4
HP <callmesammy@yahoo.com>
2023-05-01 20:26:46 +0100

    Update

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,126 +1,130 @@
-import React from 'react'
-import { useState } from 'react'
-import { categoriesData } from './../Data/CategoriesData';
-import { Listbox, Transition} from '@headlessui/react';
-import { Fragment } from 'react';
-import { FaChevronDown} from 'react-icons/fa';
-import { MdKeyboardArrowDown } from 'react-icons/md';
+import React from "react";
+import { useState } from "react";
+import { categoriesData } from "../Data/CategoriesData";
+import { Listbox, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { FaChevronDown } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 
 const YearData = [
-{title: "Sort By Year"},
-{title: "1960-1970"},
-{title: "1970-1980"},
-{title: "1990-2000"},
-{title: "2000-2010"},
-{title: "2010-2020"},
-{title: "2020-2021"},
+  { title: "Sort By Year" },
+  { title: "1960-1970" },
+  { title: "1970-1980" },
+  { title: "1990-2000" },
+  { title: "2000-2010" },
+  { title: "2010-2020" },
+  { title: "2020-2021" },
+];
 
-]
+const TimeDate = [
+  { title: "Sort By Time" },
+  { title: "1-3 Hours" },
+  { title: "3-5 Hours" },
+  { title: "5-7 Hours" },
+  { title: "7-9 Hours" },
+  { title: "9-11 Hours" },
+  { title: "11-13 Hours" },
+];
 
-const TimeDate =[
-{title: "Sort By Time"},
-{title: "1-3 Hours"},
-{title: "3-5 Hours"},
-{title: "5-7 Hours"},
-{title: "7-9 Hours"},
-{title: "9-11 Hours"},
-{title: "11-13 Hours"},
-]
-
-const RateDate =[
-{title: "Sort By Rate"},
-{title: "1 star"},
-{title: "2 star"},
-{title: "3 star"},
-{title: "4 star"},
-{title: "5 star"},
-{title: "6 star"},
-]
+const RateDate = [
+  { title: "Sort By Rate" },
+  { title: "1 star" },
+  { title: "2 star" },
+  { title: "3 star" },
+  { title: "4 star" },
+  { title: "5 star" },
+  { title: "6 star" },
+];
 
 function Filter() {
-const [Category, setCategory] = useState({title: 'Category'});
-const [Year, setYear] = useState(YearData[0]);
-const [Time, setTime] = useState(TimeDate[0]);
-const [Rate, setRate] = useState(RateDate[0]);
+  const [Category, setCategory] = useState({ title: "Category" });
+  const [Year, setYear] = useState(YearData[0]);
+  const [Time, setTime] = useState(TimeDate[0]);
+  const [Rate, setRate] = useState(RateDate[0]);
 
+  const Filter = [
+    {
+      value: Category,
+      onChange: setCategory,
+      items: categoriesData,
+    },
 
-const Filter = [
-  {
-    value: Category,
-    onChange:setCategory,
-    items:categoriesData
-  },
+    {
+      value: Year,
+      onChange: setYear,
+      items: YearData,
+    },
 
-  {
-    value: Year,
-    onChange:setYear,
-    items:YearData
-  },
+    {
+      value: Time,
+      onChange: setTime,
+      items: TimeDate,
+    },
 
-  {
-    value: Time,
-    onChange:setTime,
-    items:TimeDate
-  },
-
-{
-  value: Rate, 
-  onChange:setRate,
-  items:RateDate
-}
-]
+    {
+      value: Rate,
+      onChange: setRate,
+      items: RateDate,
+    },
+  ];
 
   return (
     <div className="my-6 bg-dyr  border-gray-800 grid md:grid-cols-4 grid-cols-2 lg:gap-12 gap-2 rounded p-6">
       {Filter.map((item, index) => (
-      <Listbox key={index} value={item.value} onChange={item.onChange}>  
-      <div className="relative">
-      <Listbox.Button className = "relative border border-gray-800 w-full text-white bg-maint rounded-lg cursor-default py-4 pl-6 pr-10 text-xs">
-        <span className="block truncate">{item.value.title}</span>
-        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none ">
-        <FaChevronDown className="w-5 h-5" aria-hidden="true"/>
-        </span> 
-        </Listbox.Button>
-      <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-        <Listbox.Options className="absolute z-10 mt-1 max-h-60  w-full bg-maint rounded-md shadow-lg py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-        {
-
-          item.items.map((iterm, i) => (
-            <Listbox.Option key={i} className={({active}) => `relative cursor-default select-none py-2 pl-10 pr-4'} cursor-default select-none relative py-2 pl-10 pr-4
-             ${active ? " bg-sub text-white" : "text-white" }` } value={iterm} >
-              {({selected}) => ( 
-                <>
-              <span className={` block truncate ${ selected ? 'font-semibold' : 'font-normal'}`}>
-                {iterm.title}
+        <Listbox key={index} value={item.value} onChange={item.onChange}>
+          <div className="relative">
+            <Listbox.Button className="relative border border-gray-800 w-full text-left text-white bg-maint rounded-lg cursor-default py-4 pl-6 pr-10 text-xs">
+              <span className="block truncate">{item.value.title}</span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none ">
+                <FaChevronDown className="w-3 h-3" aria-hidden="true" />
               </span>
-              
-              {
-                selected ? (
-                  <span className="absolute inset-y-0 left-0 flex items-center pl-3"> 
-                  <MdKeyboardArrowDown className='w-5 h-5'/>
-                  </span>
-             ) :null
-                }
-                </>
-               ) }   
-            </Listbox.Option>
-        
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-60  w-full bg-maint rounded-md shadow-lg py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {item.items.map((iterm, i) => (
+                  <Listbox.Option
+                    key={i}
+                    className={({
+                      active,
+                    }) => `relative cursor-default select-none py-2 pl-10 pr-4'} cursor-default select-none relative py-2 pl-10 pr-4
+             ${active ? " bg-sub text-white" : "text-white"}`}
+                    value={iterm}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={` block truncate ${
+                            selected ? "font-semibold" : "font-normal"
+                          }`}
+                        >
+                          {iterm.title}
+                        </span>
 
-        ))}
-         </Listbox.Options> 
-      
-      </Transition>
-    
-       
-      </div>
-      </Listbox>
-     
+                        {selected ? (
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <MdKeyboardArrowDown
+                              className="w-5 h-5"
+                              aria-aria-hidden="true"
+                            />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
       ))}
-      
     </div>
-      );  
-    }
-  
-                
+  );
+}
+
 export default Filter;
--- src/screens/Movies.js
+++ src/screens/Movies.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import Layout from '../Layout/Layout'
 import Filter from '../Components/Filter';
+import Movie from '../Components/Movie';
+import { Movies } from './../Data/MovieData';
 
-function Movies() {
+
+function MoviePage() {
   return (
     <Layout>
 
     
     <div className="min-height-screen container mx-auto px-2 my-6">
     <Filter/>
+    <p className="text-lg font-medium my-6">Total 
+    <span className="font-bold text-sub"> {Movies?.length} </span>
+    </p>
+    <div className="grid sm:mt-10 mt-5 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols-3 sm:grid-cols-2 gap-6">
+      {
+        Movies?.map((movie, index) => (
+          <Movie key={index} movie={movie}/>
+        ))
+      }
+    </div>
     </div>
  </Layout> 
  )
 }
 
-export default Movies
+export default MoviePage
