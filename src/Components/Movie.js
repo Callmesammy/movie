a6dcc9704a276a3582991f9dc443f128e6a48562
HP <callmesammy@yahoo.com>
2023-05-04 17:02:46 +0100

    update

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,14 +3,17 @@ import { FaHeart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 
+
 function Movie({movie}) {
   return (
   <>
   <div className=" border border-brd p-1 hover:scale-95 transitions relative rounded overflow-hidden">
-    <Link to ={'/shows/{movie?.name}'}  className="w-full">
-    <img src= {movie?.image}
+    <Link to ={`/shows/${movie?.name}`}  className="w-full">
+    <img
+     src={`/images/movies/${movie?.image}`}
      alt={movie?.name} 
      className="w-full h-64 object-cover" />
+    
     </Link>
     <div className="absolute flex-btn gap-2 bottom-0 right-0 left-0 bg-maint bg-opacity-60 px-4 py-3">
         <h3 className="font-semibold truncate">{movie?.category}</h3>
